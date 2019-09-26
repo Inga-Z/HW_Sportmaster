a78c6940ec842e8354494f07bc6c1b9081fc2e66
Inga-Z <z_inga@mail.ru>
2019-09-27 01:27:46 +0300

    Предитог

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-console.log("worked")
+console.log("worked");
 
-const
+
+const purchaseAmount = 3051;
+const bonuses = 100;
+const maxBonuses = Math.floor (purchaseAmount * 0.1);
+
+const appliedBonuses = maxBonuses <= 1000 ? maxBonuses : 100;
+
+console.log(appliedBonuses);
+
+const total = maxBonuses - 5;
+
+console.log(total);
+
+
+
+
+
+
+
